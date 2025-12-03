51390cf0f201ddbe72ce8db82e5e87bad3c94170
EmilieHolden <e.393@hotmail.com>
2025-12-03 22:35:16 +0100

    Extracted login to separate file, and renamed auth.js to register.js

---
+++ src/js/register.js
@@ -0,0 +1,46 @@
+import { registerUser } from "/src/js/api/register.js";
+import { loginUser } from "/src/js/api/login.js";
+import { validateForm } from "/src/js/utils.js";
+
+// REGISTER
+const registerForm = document.getElementById("register-form");
+const registerMessage = document.getElementById("register-message");
+
+if (registerForm) {
+    registerForm.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        if (!validateForm(registerForm)) return;
+
+        const userData = {
+            name: document.getElementById("fname").value.trim(),
+            email: document.getElementById("email").value.trim(),
+            password: document.getElementById("password").value,
+        };
+
+        let loading = false
+
+        try {
+            loading = true
+            registerMessage.textContent = `Registrating account...`
+
+            await registerUser(userData);
+            const loginResult = await loginUser({
+                email: userData.email,
+                password: userData.password,
+            });
+
+            localStorage.setItem("token", loginResult.data.accessToken);
+            localStorage.setItem("user", JSON.stringify(loginResult.data));
+
+            registerMessage.textContent = "Registration successful, you are now logged in to your new account";
+
+            setTimeout(() => {
+                window.location.href = "/";
+            }, 1000);
+        } catch (error) {
+            registerMessage.textContent = `Registration failed. ${error.message}`;
+            registerMessage.classList.add("error-text")
+        }
+    });
+}
